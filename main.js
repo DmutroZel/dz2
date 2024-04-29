f01f8c6df6f33e8e5762625fb0d3e5237040ad2b
DmutroZel <156704336+DmutroZel@users.noreply.github.com>
2024-04-29 16:59:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+box.onclick = function(){
+    box.style.borderRadius = "50%";
+}
+box2.onclick = function(){
+    box2.style.borderRadius = "50%";
+}
+box3.onclick = function(){
+    box3.style.borderRadius = "50%";
+}
+box4.onclick = function(){
+    box4.style.borderRadius = "50%";
+}
